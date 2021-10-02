3a4afa25ee89e141a1d517344f250fa874a6dff9
akash <akash300383@gmail.com>
2021-10-02 11:52:14 +0530

    done

---
+++ Phase 4/0.7 eBooks/Docker Images/creating image for node js application/app.js
@@ -0,0 +1,8 @@
+var a=10;
+var b=20;
+var sum = a+b;
+console.log("Sum of two number is "+sum);
+function sayHello(name){
+    return "Welcome to Simple Node JS Application "+name;
+}
+console.log(sayHello("Raj"));
